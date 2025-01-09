6f4cd9f000c225872dedfcf2c341da9ec62c4be2
Sakin790 <sakinislam79@gmail.com>
2025-01-09 11:28:20 -0500

    complete

--- Services/Inventory/src/routes/HealthRouter.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { healthCheck } from "controllers/Health";
-import { createInventory } from "@/controllers/Create";
-
-export const router = Router();
-
-router.get("/healthcheck", healthCheck);
-
-router.post("/create", createInventory);
--- Services/Inventory/src/schemas.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod";
-export const inventoryCreateDTOschema = z.object({
-  productId: z.string(),
-  sku: z.string(),
-  quantity: z.number().int().optional().default(0),
-});
-
-export type InventoryCreateDTO = z.infer<typeof inventoryCreateDTOschema>;
